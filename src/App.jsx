e4306822e855ae6950fd4f77f7d77b43dc8beca9
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-05-19 17:02:55 +0330

    start useLayout Hook Exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
-import Js_Course from "./components/Js_Course/Js_Course";
-import React_Course from "./components/React_Course/React_Course";
-import "./App.css"
-
 export default function App() {
-  return <>
-    <React_Course></React_Course>
-    <Js_Course></Js_Course>
-  </>;
+  return (
+    <div>App</div>
+  )
 }
