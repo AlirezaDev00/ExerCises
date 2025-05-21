0874f5e16f1fe2f45de92df7f171b41cfbbf5519
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-05-21 21:58:22 +0330

    Start Profiler Comp In React Exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,9 @@
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
-import "./App.css";
+import React from 'react'
 
-export default function App() {
-  const [number, setNumber] = useState(0);
-  const [divStyles, setDivStyles] = useState({});
-  const sectionRef = useRef();
-
-    //! The async code with useEffect
-    
-  //   useEffect(() => {
-  //     const randomVal = Math.floor(Math.random() * 500);
-
-  //     for (let i = 0; i <= 10_000_000; i++) {
-  //       if (i === 10_000_000) {
-  //         setDivStyles({ paddingTop: `${randomVal}px` });
-  //       }
-  //     }
-  //   }, [number]);
-
-    //* The sync code with useEffect
-    
-  useLayoutEffect(() => {
-    const randomVal = Math.floor(Math.random() * 500);
-
-    for (let i = 0; i <= 10_000_000; i++) {
-      if (i === 10_000_000) {
-        setDivStyles({ paddingTop: `${randomVal}px` });
-      }
-    }
-  }, [number]);
-
+function App() {
   return (
-    <main className="app">
-      <section ref={sectionRef} style={divStyles}>
-        <p>{number}</p>
-        <button
-          type="button"
-          onClick={() => setNumber((prevNum) => prevNum + 1)}
-        >
-          +
-        </button>
-        <button
-          type="button"
-          onClick={() => setNumber((prevNum) => prevNum + 1)}
-        >
-          -
-        </button>
-      </section>
-    </main>
-  );
+    <div>App</div>
+  )
 }
+
+export default App
